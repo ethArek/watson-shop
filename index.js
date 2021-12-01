74eb3107cc676be81dbe2c392ab819b17bed854b
Arkadiusz Sidzinka <arkadiusz.sidzinka@gmail.com>
2021-12-01 23:43:05 +0100

    feat: add greeting endpoint

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const { addMessage } = require('./watson')
+const { addMessage, getGreeting } = require("./watson");
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(express.static('static'))
+app.use(express.static("static"));
 
-app.get('/add-message', function (req, res) {
-    res.send("good")
-})
+app.post("/add-message", async function (req, res) {
+  const message = await addMessage(req.body.message);
+  res.send(message);
+});
 
-app.post('/add-message', async function (req, res) {
-  const message = await addMessage(req.body.message)
-  res.send(message)
-})
- 
-app.listen(3000, () => console.log('listening on port 3000'))
+app.get("/greeting", async function (req, res) {
+  const greeting = await getGreeting();
+  res.send(greeting);
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
